8ab21d668918df35d7fa2a5ec44fea2d0f9c3dd5
williambparrish <34019521+williambparrish@users.noreply.github.com>
2017-12-03 21:00:21 -0800

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,40 @@
+import java.util.Vector;
+
+public class Game {
+	private Vector<Page> pages;
+	private Vector<Shape> inventory;
+	private Page currentPage;
+	
+	public Game(Vector<Page> pageVec, Vector<Shape> initialInventory) {
+		pages = pageVec;
+		inventory = initialInventory;
+	}
+	
+	public Vector<Shape> getInvetory(){
+		return inventory;
+	}
+	
+	public void changePage(Page page) {
+		currentPage = page;
+	}
+	
+	public void addToInventory(Shape shape, int x, int y) {
+		inventory.add(shape);
+		shape.setInventoryStatus(true);
+		Page page = shape.getPage();
+		page.removeShape(shape);
+		shape.setPage(null);
+		moveShape(shape, x, y);
+	}
+	
+	public void removeFromInventory(Shape shape, int x, int y) {
+		inventory.remove(shape);
+		currentPage.addShape(shape);
+		moveShape(shape, x, y);
+	}
+	
+	public void moveShape(Shape shape, int x, int y) {
+		shape.setX(x);
+		shape.sety(y);
+	}
+}
