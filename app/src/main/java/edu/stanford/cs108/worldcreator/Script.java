9bf74924a4a9a6cf3f06a30db9d1f6792b8a6433
williambparrish <34019521+williambparrish@users.noreply.github.com>
2017-12-05 01:51:45 -0800

    Update Script.java

--- app/src/main/java/edu/stanford/cs108/worldcreator/Script.java
+++ app/src/main/java/edu/stanford/cs108/worldcreator/Script.java
@@ -1,6 +1,8 @@
 package edu.stanford.cs108.worldcreator;
 
 import java.util.Vector;
+import java.util.StringTokenizer;
+import android.media.MediaPlayer;
 
 public class Script{
 	public Object parent;
@@ -9,12 +11,38 @@ public class Script{
 	public Vector<Object>[] onDrop;
 	public Vector<Object>[] onEnter;
 	
-	public Script(Object obj) {
-		parent = obj;
+	
+	public Script(String input) {
 		for(int i = 0; i < 4; i++) {
 			onClick[i] = new Vector<Object>();
 			onDrop[i] = new Vector<Object>();
 			onEnter[i] = new Vector<Object>();
+		}
+		String[] strs = input.split(";");
+		for(String str : strs) {
+			str.trim();
+			StringTokenizer st = new StringTokenizer(str);
+			handleTokens(st);
+		}
+	}
+	
+	private void handleTokens(StringTokenizer st) {
+		st.nextToken(); //ignore "on" before click drop or add
+		String action = st.nextToken();
+		while(st.hasMoreTokens()) {
+			String command = st.nextToken();
+			String target = st.nextToken();
+			switch(action) {
+			case "click": 
+				addToOnClick(command, target);
+				break;
+			case "drop":
+				addToOnDrop(command, target);
+				break;
+			case "enter":
+				addToOnEnter(command, target);
+				break;
+			}
 		}
 	}
 	
@@ -22,42 +50,42 @@ public class Script{
 	public Vector<Object>[] getOnDropActions() { return onDrop;}
 	public Vector<Object>[] getOnEnterActions() { return onClick;}
 	
-	public void addToOnClick(String command, Object target) {
+	public void addToOnClick(String command, String target) {
 		switch(command) {
 		case "goto": 
-			onClick[0].add(target);
+			onClick[0].add(Game.curGame.getPage(target));
 		case "play":
-			onClick[1].add(target);
+			onClick[1].add(Game.curGame.getSound(target));
 		case "hide":
-			onClick[2].add(target);
+			onClick[2].add(Game.curGame.getShape(target));
 		case "show":
-			onClick[3].add(target);
+			onClick[3].add(Game.curGame.getShape(target));
 		}
 	}
 	
-	public void addToOnDrop(String command, Object target) {
+	public void addToOnDrop(String command, String target) {
 		switch(command) {
 		case "goto": 
-			onDrop[0].add(target);
+			onDrop[0].add(Game.curGame.getPage(target));
 		case "play":
-			onDrop[1].add(target);
+			onDrop[1].add(Game.curGame.getSound(target));
 		case "hide":
-			onDrop[2].add(target);
+			onDrop[2].add(Game.curGame.getShape(target));
 		case "show":
-			onDrop[3].add(target);
+			onDrop[3].add(Game.curGame.getShape(target));
 		}
 	}
 	
-	public void addToOnEnter(String command, Object target) {
+	public void addToOnEnter(String command, String target) {
 		switch(command) {
 		case "goto": 
-			onEnter[0].add(target);
+			onEnter[0].add(Game.curGame.getPage(target));
 		case "play":
-			onEnter[1].add(target);
+			onEnter[1].add(Game.curGame.getSound(target));
 		case "hide":
-			onEnter[2].add(target);
+			onEnter[2].add(Game.curGame.getShape(target));
 		case "show":
-			onEnter[3].add(target);
+			onEnter[3].add(Game.curGame.getShape(target));
 		}
 	}
 }
