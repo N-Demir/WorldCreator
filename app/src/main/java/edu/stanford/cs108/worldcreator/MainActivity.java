91c6a13a3354be2a9bab56d3a219af15899229c0
rdervay <rdervay7@stanford.edu>
2017-12-05 00:50:54 -0800

    database creation and parsing
    updated

--- app/src/main/java/edu/stanford/cs108/worldcreator/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/worldcreator/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
 
@@ -26,7 +28,23 @@ public class MainActivity extends AppCompatActivity {
         if (tablesCursor.getCount() == 0) {
             String gameTable = "CREATE TABLE games ("
                     + "name TEXT,"
-                    + "_id "
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT"
+                    + ");";
+            db.execSQL(gameTable);
+            String pageTable = "CREATE TABLE pages ("
+                    + "name TEXT, game TEXT,"
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT"
+                    + ");";
+            db.execSQL(pageTable);
+            // geometry 4 ints, name, movable, visible, image-name, text, associatedscript
+            String shapesTable = "CREATE TABLE shapes ("
+                    + "name TEXT, game TEXT,"
+                    + "page TEXT, x INTEGER, y INTEGER,"
+                    + "height INTEGER, width INTEGER, move INTEGER,"
+                    + "visible INTEGER, image TEXT, script TEXT, label TEXT"
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT"
+                    + ");";
+            db.execSQL(shapesTable);
         }
         //TODO:FINISH DATABASE STUFF
 
@@ -70,11 +88,43 @@ public class MainActivity extends AppCompatActivity {
      * @param intent
      */
     private void gotoActivity(Intent intent) {
+        Vector<Page> document = new Vector<Page>();
         Spinner spinner = (Spinner) findViewById(R.id.game_spinner);
         String gameName = spinner.getSelectedItem().toString();
-
-        //TODO: Fill out curGame static field in game
-
+        Cursor pCursor = db.rawQuery("SELECT * FROM pages WHERE game =" + gameName, null);
+        while (pCursor.moveToNext()){
+            String pageName = pCursor.getString(0);
+            Page cur = new Page(pCursor.getString(0));
+            // check if queries are correct
+            Cursor sCursor = db.rawQuery("SELECT * FROM shapes WHERE game='" + gameName + "' AND page = " + pageName, null);
+            while (sCursor.moveToNext()){
+                String name = sCursor.getString(0);
+                int xCord = sCursor.getInt(3);
+                int height = sCursor.getInt(5);
+                int width = sCursor.getInt(6);
+                int yCord = sCursor.getInt(4);
+                int vis = sCursor.getInt(8);
+                boolean visiblity = false;
+                boolean mover = false;
+                if (vis == 1) visiblity = true;
+                int move = sCursor.getInt(7);
+                if (move == 1) mover = true;
+                String image = sCursor.getString(9);
+                String script = sCursor.getString(10);
+                String label = sCursor.getString(11);
+                Shape s = new Shape(name, xCord, yCord);
+                s.setHeight(height);
+                s.setWidth(width);
+                s.setImageName(image);
+                s.setHidden(visiblity);
+                s.setMoveable(mover);
+                s.setText(label);
+                cur.addShape(s);
+            }
+            document.add(cur);
+        }
+        Game newGame = new Game(document, null, gameName);
+        Game.curGame = newGame;
         startActivity(intent);
     }
 }
