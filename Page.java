8fff8f9885538574fe6ce1d865e0c03908c65999
williambparrish <34019521+williambparrish@users.noreply.github.com>
2017-12-04 17:52:36 -0800

    Update Page.java

--- Page.java
+++ Page.java
@@ -9,9 +9,11 @@ public class Page {
 		this.pName = name; 
 		shapes = new ArrayList<Shape>();
 	}
+	
 	public String getName() {
 		return pName; 
 	}
+	
 	public int removeShape(Shape s) {
 		if (shapes.contains(s)) {
 			shapes.remove(s);
@@ -19,15 +21,14 @@ public class Page {
 		}
 		return 0; 
 	}
+	
 	public void addShape(Shape s) {
 		shapes.add(s); 
 	}
 	
-	// to implement loop for drawing shapes
-//	public void drawShapes() {
-//		for (int i = 0; i < shapes.size(); i++) {
-//			Shape cur = shapes[i];
-//			drawShape(cur);
-//		}
-//	}
+	public void draw() {
+		for(Shape shape: shapes) {
+			shape.draw();
+		}
+	}
 }
