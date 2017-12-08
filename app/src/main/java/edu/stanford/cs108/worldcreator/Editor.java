27c86af5fb0af201b3177af1fe494a7746326778
Nikita Demir <nikitde1@gmail.com>
2017-12-07 20:30:05 -0800

    One more

--- app/src/main/java/edu/stanford/cs108/worldcreator/Editor.java
+++ app/src/main/java/edu/stanford/cs108/worldcreator/Editor.java
@@ -75,7 +75,7 @@ public class Editor extends AppCompatActivity {
         curShape.setMovable(((RadioButton)findViewById(R.id.movable)).isChecked());
         curShape.setHidden(((RadioButton)findViewById(R.id.notVisible)).isChecked());
 
-        findViewById(R.id.EditorView).invalidate(); //TODO:IMPLEMENT EVERYWHERE?
+        findViewById(R.id.EditView).invalidate(); //TODO:IMPLEMENT EVERYWHERE?
     }
 
     private void setShapeFields(Shape shape){
